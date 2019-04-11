7f6f20f59da60f81d4804cd84547fd06a028adf5
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-11 17:07:01 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winGame = array.find (game => game.result === "W")
+  if (winGame) {
+    return winGame.year
+  }
+}
